7e9824d35444e27c292f5405d6fddf2f8f4443ae
ajmalshukoor <ajmalshukoor11@gmail.com>
2023-01-14 10:38:25 -0500

    final

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import { TravelContextProvider } from './context/TravelContext';
 import { AuthContextProvider } from './context/AuthContext';
-import { ImageContextProvider } from './context/ImageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
     <TravelContextProvider>
-      <ImageContextProvider>
         <App />
-      </ImageContextProvider>
     </TravelContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
